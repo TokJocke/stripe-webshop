3d13b6d9fdd95731f478f197edcebc810d2bef05
Sebastian Borg <Sebastian.borg99@hotmail.se>
2021-09-23 15:47:47 +0200

    url params

--- client/src/components/contentView.tsx
+++ client/src/components/contentView.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, useParams } from 'react-router-dom';
 import Cart from './cart';
 import Main from './main';
 
@@ -8,14 +8,14 @@ import Main from './main';
 
 export default function ContentView() {
 
-
+    
 
 
     return (
         
             <Switch>
-                <Route exact path={"/"} component={Main} />       {/* trying to add dynamic region to url */}                    
-                <Route path={"/kundvagn"} component={Cart} />
+                <Route exact path={"/:username?"} component={Main}></Route>                  
+                <Route exact path={"/:username/kundvagn"} component={Cart} />
             </Switch>                
     
     )
