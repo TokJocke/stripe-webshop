1ab91108d7a43f51cbb4db6679cd1b8bdc35a64f
Sebastian Borg <Sebastian.borg99@hotmail.se>
2021-09-28 16:27:02 +0200

    auth

---
+++ client/src/components/authError.tsx
@@ -0,0 +1,19 @@
+import { useStripe } from '@stripe/react-stripe-js';
+import React, { CSSProperties, useState } from 'react';
+import InCartList from './inCartList';
+
+
+
+export default function AuthError401() {
+
+
+    return (
+        <div>
+          
+             Du är inte inloggad!
+        
+        </div>
+                
+    );
+}
+
